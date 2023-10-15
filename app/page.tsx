b5a0f42a03fdb81fc25bd96bc151066a02da541c
Srinjoy003 <srinjoy003@gmail.com>
2023-10-16 03:09:38 +0530

    set up pieces

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,22 @@ import { v4 as uuidv4 } from "uuid";
 function CreateBoardMap() {
 	const board = [];
 
-	for (let i = 1; i <= 8; i++) {
+	for (let i = 0; i <= 7; i++) {
 		let row = [];
-		for (let j = 1; j <= 8; j++) {
-			row.push("_");
+		for (let j = 0; j <= 7; j++) {
+			if (i == 1 || i == 6) row.push("P");
+			else if (i == 0 || i == 7) {
+				if (j == 0 || j == 7) row.push("R");
+				else if (j == 1 || j == 6) row.push("H");
+				else if (j == 2 || j == 5) row.push("B");
+				else if(j == 3) row.push("Q");
+				else if(j == 4) row.push("K");
+
+			}
+			else
+				row.push("_");
 		}
+		
 		board.push(row);
 	}
 
@@ -30,11 +41,20 @@ function CreateBoard(boardMap: string[][]) {
 			return (
 				<div
 					className={`flex flex-row w-20 h-20 border-2 border-black text-5xl items-center justify-center ${
-						(i + j) % 2 ? "bg-amber-950" : "bg-white"
-					}`}
+						(i + j) % 2 ? "bg-amber-950" : "bg-slate-500"
+					} ${i > 5 ? "text-white": "text-black"}`}
 					key={i * j}
 				>
-					<FaChessBishop />
+					{col == "P" && <FaChessPawn />}
+					{col == "R" && <FaChessRook />}
+					{col == "K" && <FaChessKing />}
+					{col == "H" && <FaChessKnight />}
+					{col == "Q" && <FaChessQueen />}
+					{col == "B" && <FaChessBishop />}
+
+
+
+
 				</div>
 			);
 		});
@@ -51,8 +71,6 @@ function CreateBoard(boardMap: string[][]) {
 export default function Home() {
 	const boardMap = CreateBoardMap();
 	const board = CreateBoard(boardMap);
-
-
 
 	return (
 		<div className="flex flex-col w-screen h-screen items-center justify-center bg-slate-700">
